7a3f682f0f8523a058260681981f5452680854b9
ej <evanrjohnso@gmail.com>
2017-09-08 13:19:55 -0700

    browserSync loading correctly

--- js/interface.js
+++ js/interface.js
@@ -1,10 +1,7 @@
 $(document).ready(function() {
-alert('page loaded');
 $('#time').text(moment());
   $("#birthday").submit(function(e) {
     e.preventDefault();
-    var bday = $("#bday").val();
-    console.log(bday);
-    alert('hey');
+    var bday = $("#bday").val(); //XXXX-XX-XX
   });
 });
